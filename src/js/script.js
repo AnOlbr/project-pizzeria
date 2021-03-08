0176e0f20c33d19e27d5cf40bbd2dec64ae5798e
Aneta Olbromska <anetaolbromska@gmail.com>
2021-03-08 13:35:21 +0100

    Add pictures

--- src/js/script.js
+++ src/js/script.js
@@ -86,6 +86,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -141,26 +142,35 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        //console.log(paramId, param);
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          const optionImage = thisProduct.imageWrapper.querySelector('".'+ paramId + '-' + optionId + '"');
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if(optionSelected) {
             // check if the option is not default
             if(option.default == false) {
               // add option price to price variable
               price += option.price;
             }
           } else {
+            //image.classList.remove('active');
             // check if the option is default
             if(option.default == true) {
               // reduce price variable
               price -= option.price;
             }
-          } 
+          }
+          
+          if(optionImage){
+            if(optionSelected){
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+          }
         }
       }   
       // update calculated price in the HTML
