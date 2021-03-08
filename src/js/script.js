95495d6fedb4839672e1c80c26aa65fb18fba58d
Aneta Olbromska <anetaolbromska@gmail.com>
2021-03-08 12:30:58 +0100

    Fix price counting

--- src/js/script.js
+++ src/js/script.js
@@ -60,13 +60,14 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
       thisProduct.processOrder();
-      console.log('new Product:', thisProduct);
+     // console.log('new Product:', thisProduct);
     }
 
     renderInMenu() {
       const thisProduct = this;
-      console.log(thisProduct.data);
+     // console.log(thisProduct.data);
       /*generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
       /*create element using utils.createElementFromHTML */
@@ -92,14 +93,14 @@
 
       /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log(clickableTrigger);
+      //console.log(clickableTrigger);
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log(activeProduct);
+        //console.log(activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct !==null && activeProduct !== thisProduct.element){
           activeProduct.classList.remove('active');
@@ -131,26 +132,21 @@
 
     processOrder() {
       const thisProduct = this;
-    
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
-    
+      //console.log('formData', formData);
       // set price to default price
       let price = thisProduct.data.price;
-    
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
-    
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           console.log(optionId, option);
-
           // check if there is param with a name of paramId in formData and if it includes optionId
           if(formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
@@ -166,8 +162,7 @@
             }
           } 
         }
-      }
-    
+      }   
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
@@ -176,7 +171,7 @@
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
@@ -191,12 +186,11 @@
 
     init: function () {
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-
+      //console.log('*** App starting ***');
+      //console.log('thisApp:', thisApp);
+      //console.log('classNames:', classNames);
+      //console.log('settings:', settings);
+      //console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
     },
