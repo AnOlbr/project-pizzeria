cbf94aa067f2b3d24e9c059792961961f9b8c5b4
Aneta Olbromska <anetaolbromska@gmail.com>
2021-04-02 19:44:40 +0200

    Add subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,56 @@
-import {settings, select} from '/js/settings.js';
+import {settings, select, classNames} from '/js/settings.js';
 import Product from '/js/components/Product.js';
 import Cart from '/js/components/Cart.js';
 
 
 const app = {
+
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#','');
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if (page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /*get page id from href attribute*/
+        const id = clickedElement.getAttribute('href').replace('#','');
+        /*run thisApp.activatePage with that id*/
+        thisApp.activatePage(id); 
+        /*change URL hash*/
+        window.location.hash = '#/' + id; 
+      });  
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#/'+ pageId
+      );
+    }
+  },
+
   initData: function () {
     const thisApp = this;
     thisApp.data = {};
@@ -13,14 +60,11 @@ const app = {
         return rawResponse.json();
       })
       .then(function (parsedResponse) {
-        console.log('parsedResponse', parsedResponse);
         /*save parseResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /*execute initMenu method*/
         app.initMenu();
       });
-
-    console.log('thisapp.data', JSON.stringify(thisApp.data));
   },
 
   initMenu: function () {
@@ -34,6 +78,7 @@ const app = {
   init: function () {
     const thisApp = this;
 
+    thisApp.initPages();
     thisApp.initData();
   },
 
