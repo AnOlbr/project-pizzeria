356c7971e8e85537435df673c514837c902a7dac
Aneta Olbromska <anetaolbromska@gmail.com>
2021-04-06 18:30:11 +0200

    Add full booking to be fixed

--- src/js/settings.js
+++ src/js/settings.js
@@ -62,6 +62,12 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    allTables: '.floor-plan',
+    form: '.booking-form',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+    starters: '[name="starter"]',
+
   },
   nav: {
     links: '.main-nav a',
@@ -79,6 +85,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
